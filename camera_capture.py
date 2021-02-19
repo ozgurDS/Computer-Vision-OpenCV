96902c18391602c349cddd511f3cc4e09dfa6527
Özgür Hacımustafaoğlu <73536286+Narsilion909@users.noreply.github.com>
2021-02-19 17:36:47 +0300

    Update camera_capture.py

--- camera_capture.py
+++ camera_capture.py
@@ -2,17 +2,14 @@ import cv2
 import numpy as np
 
 cap = cv2.VideoCapture(0)
-cap.set(3,640)   #Changing width to 640
-cap.set(4,480)   #Changing height to 480
-ca.set(10,100)   #Changing brightness to 100
-
+cap.set(3,640)    #Changing width to 640
+cap.set(4,480)    #Changing height to 480
+cap.set(10,100)   #Changing brightness to 100
 
 while True:
-    
-    ret, frame = cap.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    cv2.imshow("Capture", gray)
-
+    success, frame = cap.read()
+    #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)   ##If we want gray capture 
+    cv2.imshow("Capture", frame)
     if cv2.waitKey(30) & 0xFF == ord("q"):
         break
 
